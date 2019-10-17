38b21f53e58eb82762d4a143f8784b4a2476ff6f
Alberto Calvo <albertocalvojaca@gmail.com>
2019-10-17 21:46:47 +0200

    fibonacci falla escenario 3

--- fibonaccis.exs
+++ fibonaccis.exs
@@ -1,9 +1,9 @@
-# AUTORES:
-# fuentes: 
-# FICHERO: fibonacci.exs
-# FECHA: 
+# AUTORES: Oscar Baselga Lahoz y Alberto Calvo Rubió
+# NIAs: 760077 y 760739
+# FICHERO: fibonaccis.exs
+# FECHA: 15/10/19
 # TIEMPO: 
-# DESCRIPCION: 
+# DESCRIPCION: modulos fibonacci, cliente, master, worker para un despliegue de un sistema
 
 defmodule Fib do
 	def fibonacci(0), do: 0
@@ -31,41 +31,46 @@ defmodule Fib do
 end	
 
 defmodule Cliente do
-
-  def launch(pid, op, 1) do
-	send(pid, {self(), op, 1..36, 1})
-	receive do 
-		{:result, l} -> l
-	end
+    def launch(pid, op, 1) do
+        tiempoInicial = Time.utc_now
+        pidEscuchar = spawn(fn -> escuchar(tiempoInicial) end)
+	    send(pid, {pidEscuchar, op, 1..36, 1})
+    
   end
 
-  def launch(pid, op, n) when n != 1 do
-    spawn fn -> launch(pid, op, 1) end
+def launch(pid, op, n) when n != 1 do
+    tiempoInicial = Time.utc_now
+    pidEscuchar = spawn(fn -> escuchar(tiempoInicial) end)
+    send(pid, {pidEscuchar, op, 1..36,n})
 	launch(pid, op, n - 1)
   end 
+
+  def escuchar(tiempoInicial) do
+    receive do
+        {:result, l} -> l
+    end
+    tiempoTotal = Time.diff(Time.utc_now, tiempoInicial, :milliseconds)
+    IO.puts("Tiempo total ---> #{tiempoTotal}")
+  end
   
   def genera_workload(server_pid, escenario, time) do
-    tiempo1 = Time.utc_now
 	cond do
 		time <= 3 ->  launch(server_pid, :fib, 8); Process.sleep(2000)
 		time == 4 ->  launch(server_pid, :fib, 8);Process.sleep(round(:rand.uniform(100)/100 * 2000))
 		time <= 8 ->  launch(server_pid, :fib, 8);Process.sleep(round(:rand.uniform(100)/1000 * 2000))
 		time == 9 -> launch(server_pid, :fib_tr, 8);Process.sleep(round(:rand.uniform(2)/2 * 2000))
 	end
-    tiempoTotal = Time.diff(Time.utc_now, tiempo1, :milliseconds)
-    IO.puts("Tiempo total: #{tiempoTotal}")
+    
   	genera_workload(server_pid, escenario, rem(time + 1, 10))
   end
 
   def genera_workload(server_pid, escenario) do
-    tiempo1 = Time.utc_now
+    
   	if escenario == 1 do
 		launch(server_pid, :fib, 1)
 	else
 		launch(server_pid, :fib, 4)
 	end
-    tiempoTotal = Time.diff(Time.utc_now, tiempo1, :milliseconds)
-    IO.puts("Tiempo total: #{tiempoTotal}")
 	Process.sleep(2000)
   	genera_workload(server_pid, escenario)
   end
@@ -81,75 +86,68 @@ defmodule Cliente do
 end
 
 defmodule Worker do
-	def initWorker(pidPool) do
+	def initWorker() do
         receive do
-            {:peticion, pidCliente, op, lista, n} -> work(pidPool, pidCliente, lista, op, n)
+            {:peticion, pidEscuchar, pidPool, op, lista, n} -> work(pidEscuchar, pidPool, lista, op, n)
         end
     end
 
-    def work(pidPool, pidCliente, listaCalcular, op, veces) do
+    def work(pidEscuchar, pidPool, listaCalcular, op, veces) do
         if op == :fib do
             tiempo1 = Time.utc_now
             resultado = Enum.map(listaCalcular, fn x -> Fib.fibonacci(x) end)
             tiempoEjecucion = Time.diff(Time.utc_now, tiempo1, :milliseconds)
             IO.puts("Tiempo ejecucion: #{tiempoEjecucion}")
-            send(pidCliente, {:result, resultado})
+            send(pidEscuchar, {:result, resultado})
         else
             tiempo1 = Time.utc_now
             resultado = Enum.map(listaCalcular, fn x -> Fib.fibonacci_tr(x) end)
             tiempoEjecucion = Time.diff(Time.utc_now, tiempo1, :milliseconds)
             IO.puts("Tiempo ejecucion: #{tiempoEjecucion}")
-            send(pidCliente, {:result, resultado})
+            send(pidEscuchar, {:result, resultado})
         end
-        send(pidPool, {:fin, self()})
-        IO.puts("fin worker")
+        send(pidPool, {:fin, node()})
     end
 end
 
 
 defmodule Pool do
 
-    # Almacena la informacion de las maquinas worker (pasadas como un map[id,4]) y llama a controlarWorkers()
     def initPool(maquinas_workers,carga_maquinas) do
         bestWorker = Enum.min(carga_maquinas)
-        IO.inspect(carga_maquinas, label: "carga1: ")
-        #carga_maquinas = List.update_at(carga_maquinas, Enum.find_index(carga_maquinas, fn x -> x == pidWorker end), &(&1 - 1));
         if bestWorker == 4 do
             receive do
-                {:fin,pidWorker} -> IO.inspect(carga_maquinas, label: "carga4: ")
+                {:fin,nodeWorker} -> initPool(maquinas_workers,List.update_at(carga_maquinas, Enum.find_index(maquinas_workers, fn x -> x == nodeWorker end), &(&1 - 1)))
+            end
+        else
+            indexBestWorker = Enum.find_index(carga_maquinas, fn x -> x == bestWorker end)
+            bestWorker = Enum.at(maquinas_workers,indexBestWorker)
+            pidBestWorker = Node.spawn(bestWorker, fn -> Worker.initWorker() end)
+            # IO.puts("bestWorker: #{bestWorker}") # Ver que son de maquinas diferentes
+            receive do
+                {:fin,nodeWorker} -> initPool(maquinas_workers,List.update_at(carga_maquinas, Enum.find_index(maquinas_workers, fn x -> x == nodeWorker end), &(&1 - 1)))
+                {:request, pidAtenderMaster} -> send(pidAtenderMaster,{:reply,pidBestWorker});initPool(maquinas_workers, List.update_at(carga_maquinas, indexBestWorker, &(&1 + 1)))
+                
             end
         end
-        IO.inspect(carga_maquinas, label: "carga2: ")
-
-        indexBestWorker = Enum.find_index(carga_maquinas, fn x -> x == bestWorker end)
-        bestWorker = Enum.at(maquinas_workers,indexBestWorker)
-
-        pidBestWorker = Node.spawn(bestWorker, fn -> Worker.initWorker(self()) end)
-
-        carga_maquinas = List.update_at(carga_maquinas, indexBestWorker, &(&1 + 1))
-        IO.puts("bestWorker: #{bestWorker}")
-        receive do
-            {pidAtenderMaster,:request} -> send(pidAtenderMaster,{:reply,pidBestWorker})
-            {:fin,pidWorker} -> IO.inspect(carga_maquinas, label: "carga4: ")
-        end
-        IO.inspect(carga_maquinas, label: "carga3: ")
-        initPool(maquinas_workers,carga_maquinas)
     end
 end
 
 defmodule Master do
 
-    def atender(pidPool, pidCliente, op, lista, n) do
-        send(pidPool, {self(), :request})
+    def atender(pidEscuchar, pidPool, op, lista, n) do
+        # Peticion de worker a Pool
+        send(pidPool, {:request, self()})
+        # Recepcion del worker y envio de trabajo
         receive do
-            {:reply, pidWorker} -> send(pidWorker, {:peticion, pidCliente, op, lista, n})
+            {:reply, pidWorker} -> send(pidWorker, {:peticion, pidEscuchar, pidPool, op, lista, n})
         end
     end
 
     # Inicializacion del sistema empezando por el Master
     def initMaster(pidPool) do
         receive do
-            {pidCliente, op, lista, n} -> spawn(fn -> atender(pidPool, pidCliente, op, lista, n) end)
+            {pidEscuchar, op, lista, n} -> spawn(fn -> atender(pidEscuchar, pidPool, op, lista, n) end)
         end
         initMaster(pidPool)
     end
